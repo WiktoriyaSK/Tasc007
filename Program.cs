81c8b153ddb8772507a633487e551c1d59bb6400
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-15 23:45:31 +0300

    zadacha 47

--- Program.cs
+++ Program.cs
@@ -1,21 +1,47 @@
-﻿int[,] ChangeArray(int[,] array)
+﻿/*
+Задача 47. 
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(double[,] array)
 {
-    if (array.GetLength(0) != array.GetLength(1)) 
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.WriteLine("Ошибка: невозможно заменить строки на столбцы. Введите одинаковое число строк и столбцов");
-        return array;
-    }
-    else
-    {
-        int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                newArray[i, j] = array[j, i];
-            }
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
         }
-        return newArray;
     }
 }
 
+//  Функция вывода массива в терминал
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Функция ввода 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
