72e58b1e06ad2e0edc1c3d80a9919aa539110007
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-15 23:48:38 +0300

    zadacha 50

--- Program.cs
+++ Program.cs
@@ -1,32 +1,38 @@
 ﻿/*
-Задача 47. 
-Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9
-*/
+ Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+ значение этого элемента или же указание, что такого элемента нет.
+ Во вводе первая цифра - номер строки, вторая - столбца. Цифры не больше 9.
+ Например, задан массив:
+ 1 4 7 2
+ 5 9 2 3
+ 8 4 2 4
+ 17 -> такого числа в массиве нет
+ */
 
-int rows = ReadInt("Введите количество строк: ");
-int colums = ReadInt("Введите количество столбцов: ");
-double[,] numbers = new double[rows, colums];
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 8];
 FillArray2D(numbers);
 PrintArray2D(numbers);
 
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(double[,] array)
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+
+// Заполнение массива рандомными числами от 1 до 9
+void FillArray2D(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(-99, 99) / 10.0;
+            array[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
 //  Функция вывода массива в терминал
-void PrintArray2D(double[,] array)
+void PrintArray2D(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
